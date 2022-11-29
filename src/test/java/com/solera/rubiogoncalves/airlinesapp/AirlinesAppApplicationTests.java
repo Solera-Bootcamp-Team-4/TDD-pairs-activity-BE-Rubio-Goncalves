11474e2de3a66cc71062c60acf625f1f75cc080e
Adrià Rubio Coll <adria.rubio@solera.com>
2022-11-29 10:05:44 +0100

    Generate project

---
+++ src/test/java/com/solera/rubiogoncalves/airlinesapp/AirlinesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solera.rubiogoncalves.airlinesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
