b248ee585a35566ab0846394c195de415a40753b
Adrià Rubio Coll <adria.rubio@solera.com>
2022-12-01 13:31:55 +0100

    Establish a connection to the cluster
    I needed to create a new user on the cluster. The admin user gave me authentication errors.

--- src/main/java/com/solera/rubiogoncalves/airlinesapp/AirlinesAppApplication.java
+++ src/main/java/com/solera/rubiogoncalves/airlinesapp/AirlinesAppApplication.java
@@ -2,12 +2,14 @@ package com.solera.rubiogoncalves.airlinesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AirlinesAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AirlinesAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinesAppApplication.class, args);
+    }
 
 }
